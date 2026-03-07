d72bd99fb3ef158fc7d8edcc720352ba75d0a112
fatema <162889012+Fatema-AlHashem@users.noreply.github.com>
2026-03-07 18:13:22 +0300

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+from src.state import wait_and_capture_board,detect_peices,Create_board,Human_turn,decision,get_cell,move_to_target
+
+
+frame=wait_and_capture_board()
+
+Detection=detect_peices(frame)
+
+Board=Create_board(Detection)
+
+Decisison=decision(Board)
 
